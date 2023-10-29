f01da216c82230bd8d76a678c0755433a497ca60
py4ds_66056068 <66056068@kmitl.ac.th>
2023-10-29 12:01:26 +0700

    SanFran commit

--- newapp.py
+++ newapp.py
@@ -29,16 +29,20 @@ df_dbh_grouped = pd.DataFrame(
     trees_df.groupby(['dbh']).count()['tree_id'])
 df_dbh_grouped.columns = ['tree_count']
 
-tab1, tab2, tab3 = st.tabs(["Line Chart", "Bar Chart", "Area Chart"])
-with tab1:
-    st.line_chart(df_dbh_grouped)
-with tab2:
-    st.bar_chart(df_dbh_grouped)
-with tab3:
-    st.area_chart(df_dbh_grouped)
+# tab1, tab2, tab3 = st.tabs(["Line Chart", "Bar Chart", "Area Chart"])
+# with tab1:
+#     st.line_chart(df_dbh_grouped)
+# with tab2:
+#     st.bar_chart(df_dbh_grouped)
+# with tab3:
+#     st.area_chart(df_dbh_grouped)
 
-st.caption('กราฟ แสดงจำนวนต้นไม้ จัดกลุ่มตามเส้นผ่าศุนย์กลาง')
-st.title('แปลผล')
-st.write("""
-ส่วนใหญ่ของต้นไม้ใน SF มีเส้นผ่าศุนย์กลาง 3' (2,721 ต้น)
-""")
+# st.caption('กราฟ แสดงจำนวนต้นไม้ จัดกลุ่มตามเส้นผ่าศุนย์กลาง')
+# st.title('แปลผล')
+# st.write("""
+# ส่วนใหญ่ของต้นไม้ใน SF มีเส้นผ่าศุนย์กลาง 3' (2,721 ต้น)
+# """)
+
+trees_df = trees_df.dropna(subset=['longtitude', 'latitude'])
+trees_df = trees_df.sample(n=1000, replace= True)
+st.map(trees_df)
